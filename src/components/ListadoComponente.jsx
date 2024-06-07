14d5a6793efaff569dc10a6bc045c468c312bf83
Peralta, Mariela <marielajperalta@gmail.com>
2024-06-07 01:33:20 -0300

    Se agrega modal de confirmacion de borrado

--- src/components/ListadoComponente.jsx
+++ src/components/ListadoComponente.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react"
 import { ModalComponente } from "./ModalComponente"
+import { ModalConfirmarBorradoComponente } from "./ModalConfirmarBorradoComponente"
 
 export const ListadoComponente = ({ productos, isLoading, errors, deleteProducto, updateProducto }) => {
 
-    const [showModal, setShowModal] = useState(false);
+    const [showModal, setShowModal] = useState(false)
+    const [showDeleteModal, setShowDeleteModal] = useState(false)
     const [selectedId, setSelectedId] = useState()
 
     const calcularTotal = () => {
@@ -11,12 +13,22 @@ export const ListadoComponente = ({ productos, isLoading, errors, deleteProducto
     }
 
     // Ordenar productos por ID de manera ascendente
-    const productosOrdenados = [...productos].sort((a, b) => a.id - b.id);
+    const productosOrdenados = [...productos].sort((a, b) => a.id - b.id)
 
 
     const handleUpdateClick = (productoId) => {
         setSelectedId(productoId)
         setShowModal(true)
+    }
+
+    const handleDeleteClick = (productoId) => {
+        setSelectedId(productoId)
+        setShowDeleteModal(true)
+    }
+
+    const handleDeleteConfirm = (productoId) => {
+        deleteProducto(productoId)
+        setShowDeleteModal(false)
     }
 
     return (
@@ -27,6 +39,13 @@ export const ListadoComponente = ({ productos, isLoading, errors, deleteProducto
                 setShowModal={setShowModal}
                 productoId={selectedId}
                 onUpdate={updateProducto}
+            />
+
+            <ModalConfirmarBorradoComponente
+                show={showDeleteModal}
+                onHide={() => setShowDeleteModal(false)}
+                onConfirm={handleDeleteConfirm}
+                productoId={selectedId}
             />
             {isLoading ? (
                 <h4>Cargando...</h4>
@@ -54,7 +73,7 @@ export const ListadoComponente = ({ productos, isLoading, errors, deleteProducto
 
                                 <td>
                                     <button type="button" className="btn btn-warning btn-sm" onClick={() => handleUpdateClick(producto.id)}>Modificar</button>
-                                    <button type="button" className="btn btn-danger btn-sm" onClick={() => deleteProducto(producto.id)}>Borrar</button>
+                                    <button type="button" className="btn btn-danger btn-sm" onClick={() => handleDeleteClick(producto.id)}>Borrar</button>
                                 </td>
                             </tr>
                         ))}
