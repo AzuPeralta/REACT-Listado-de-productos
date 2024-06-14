ece66ec73018388e4816c7139e90ebae9b2bc5bb
Peralta, Mariela <marielajperalta@gmail.com>
2024-06-13 22:50:42 -0300

    Modificaciones de texto y estilos

--- src/routes/components/NavBar.jsx
+++ src/routes/components/NavBar.jsx
@@ -6,8 +6,11 @@ export const NavBar = () => {
         <>
             <nav className="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark">
                 <div className="container-fluid">
-                    <Link className="navbar-brand" to="#">Listados</Link>
-                    <div className="navbar-collapse" id="navbarNav">
+                    <Link className="navbar-brand" to="/">Api Rest de productos y comercios </Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav">
                             <li className="nav-item">
                                 <NavLink to='/' className="nav-link">Home</NavLink>
