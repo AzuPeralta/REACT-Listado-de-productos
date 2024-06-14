ac1fe53c9d08174cf971363f82b5ebe475d329da
Peralta, Mariela <marielajperalta@gmail.com>
2024-06-13 22:45:19 -0300

    Se modifica estilo del NavBar

--- src/routes/components/NavBar.jsx
+++ src/routes/components/NavBar.jsx
@@ -4,13 +4,10 @@ import { Link, NavLink } from 'react-router-dom'
 export const NavBar = () => {
     return (
         <>
-            <nav className="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+            <nav className="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark">
                 <div className="container-fluid">
-                    <Link className="navbar-brand" href="#">Listados</Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
+                    <Link className="navbar-brand" to="#">Listados</Link>
+                    <div className="navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav">
                             <li className="nav-item">
                                 <NavLink to='/' className="nav-link">Home</NavLink>
